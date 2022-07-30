42ac5ed92a190e7967d3d96849b6fb7dae1cd40b
Thibault Viglino <thibault@thvi.ch>
2022-07-30 18:14:06 +0200

    various bug fixes

--- src/server/app.py
+++ src/server/app.py
@@ -7,24 +7,28 @@ from stretchable_clock import StretcheableClock
 app = Flask(__name__)
 
 
-START_TIME = time(0, 0)
-HOUR_DURATION_IN_MINUTES = 45
+HIDE_SECONDS = True
+NUMBER_OF_HOURS_PER_DAY = 12
+START_TIME = time(1, 0)
+DAY_DURATION_IN_MINUTES = 45
 SCHEDULE = [
     (3, "Réveil / café"),
-    (8, "Récupération de carte de pointage"),
+    (8, "Horoscope"),
     (16, "Shift de travail"),
     (5, "Repas"),
     (3, "Nuit de sommeil"),
 ]
 
-clock = StretcheableClock(START_TIME, HOUR_DURATION_IN_MINUTES)
-scheduler = Scheduler(SCHEDULE)
+clock = StretcheableClock(START_TIME, DAY_DURATION_IN_MINUTES / NUMBER_OF_HOURS_PER_DAY)
+scheduler = Scheduler(SCHEDULE, NUMBER_OF_HOURS_PER_DAY)
 
 @app.route("/get_data")
 def get_time():
     time = clock.get_time()
     schedule = scheduler.get_list_of_tasks(time)
 
-    response = flask.jsonify({'time': time.isoformat(), "schedule": schedule})
+    response = flask.jsonify(
+        {'time': time.isoformat(), "schedule": schedule, "hide_seconds": HIDE_SECONDS}
+    )
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
--- src/server/schedule.py
+++ src/server/schedule.py
@@ -1,41 +1,68 @@
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, time
 from typing import List, Tuple
 
 @dataclass
 class Scheduler:
     """A class representing the workers schedule"""
 
-    def __init__(self, schedule: List[Tuple[float, str]]) -> None:
+    def __init__(
+        self,
+        schedule: List[Tuple[float, str]],
+        number_of_hours_per_day: int,
+    ) -> None:
         """The schedule is a dictionary of task duration and task name.
         The duration as no unit, is just a proportion.
         The duration will be determined so that the list of task is over one hour."""
 
-        self._schedule = self._convert_durations_into_datetimes(schedule)
+        self._number_of_hours_per_day = number_of_hours_per_day
+        self._stretch_factor = self._get_stretch_factor(schedule)
+        self._schedule = self._convert_durations_into_minutes(schedule)
 
-    def _convert_durations_into_datetimes(self, schedule: List[Tuple[float, str]]) -> List[Tuple[float, str]]:
+    def _get_stretch_factor(self, schedule: List[Tuple[float, str]]) -> float:
         """Convert the durations into minutes"""
         
         durations = list(zip(*schedule))[0]
         total_duration = sum(durations)
 
-        stretch_factor = 60 / total_duration
+        return self._number_of_hours_per_day * 60 / total_duration
 
-        return [(duration * stretch_factor, task) for duration, task in schedule]
+    def _convert_durations_into_minutes(self, schedule: List[Tuple[float, str]]) -> List[Tuple[float, str, str]]:
+        """Convert the durations into minutes"""
 
-    def _get_task_index_for_minute(self, minute: int) -> int:
+        out = []
+        acc_duration = 0
+        for duration, task in schedule:
+            out.append((
+                duration * self._stretch_factor,
+                task,
+                self._get_corrected_time_text(acc_duration * self._stretch_factor),
+            ))
+            acc_duration += duration
+
+        return out
+
+    def _get_corrected_time_text(self, minutes_after_start: float) -> str:
+        """Add the start time to the task text"""
+        hours_after_start = int(minutes_after_start / 60)
+        minutes_after_hour = int(minutes_after_start % 60)
+        return time(hours_after_start, minutes_after_hour).strftime('%H:%M')
+
+    def _get_task_index_for_minute(self, current_time: datetime) -> int:
         """Get the index of the task that should be done at a given minute"""
+        minutes = current_time.hour * 60 + current_time.minute
         acc = 0
+        
         for i in range(len(self._schedule)):
-            if minute <= acc + self._schedule[i][0]:
+            if minutes <= acc + self._schedule[i][0] - 1:                
                 return i
             acc += self._schedule[i][0]
 
-
-    def get_list_of_tasks(self, current_time: datetime) -> List[Tuple[str, bool]]:
-        """Get a list of tasks and their status"""
-        current_index = self._get_task_index_for_minute(current_time.minute)
+    def get_list_of_tasks(self, current_time: datetime) -> List[Tuple[str, str, bool]]:
+        """Get a list of tasks and their status
+        Returns a list of tuples of the form (time_text, task_text, is_done)"""
+        current_index = self._get_task_index_for_minute(current_time)
         return [
-            (task, i == current_index)
-            for i, (_, task) in enumerate(self._schedule)
+            (time_text, task, i == current_index)
+            for i, (_, task, time_text) in enumerate(self._schedule)
         ]
